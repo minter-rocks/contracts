ed15045c9e263f2f516e7eb9cc02c6f04c8baad7
r.nouripoor <r.nouripoor@gmail.com>
2022-03-06 19:00:08 +0330

    deployed to rinkeby

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,25 +1,11 @@
 const { ethers, upgrades } = require("hardhat");
 
   async function main() {
-    // // Deploying by proxy
-    // const IDCard = await ethers.getContractFactory("IDCard");
-    // const IDC = await upgrades.deployProxy(IDCard);
-    // await IDC.deployed();
-    // console.log("IDCard proxy address:", IDC.address);
-  
-  
-    // // Upgrading
-    // const IDCard = await ethers.getContractFactory("IDCard");
-    // const upgraded = await upgrades.upgradeProxy(IDC.address, IDCard);
-    // console.log("upgraded successfully.");
-
-
     // simple deploy
-    const IDCard = await ethers.getContractFactory("IDCard");
-    const IDC = await IDCard.deploy();
-    await IDC.deployed();
-    console.log("IDCard Contract Address:", IDC.address);
-    
+    const NFTFactory = await ethers.getContractFactory("NFTFactory");
+    const Factory = await NFTFactory.deploy();
+    await Factory.deployed();
+    console.log("NFTFactory Contract Address:", Factory.address); 
   }
     
   main();
