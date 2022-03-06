8901dbef6a48b7c2c0e0c68b1e5b0507410fdf9e
r.nouripoor <r.nouripoor@gmail.com>
2022-03-06 17:47:35 +0330

    constructors => initializer

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -1,21 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
 
-contract NFT is ERC721, Ownable {
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIdCounter;
+contract NFT is Initializable, ERC721Upgradeable, OwnableUpgradeable {
+    using CountersUpgradeable for CountersUpgradeable.Counter;
+    CountersUpgradeable.Counter private _tokenIdCounter;
 
     string private baseURI;
 
-    constructor(
-        string memory _name,
-        string memory _symbol,
+    function initialize(
+        string memory name_,
+        string memory symbol_,
         string memory baseURI_
-    ) ERC721(_name, _symbol) {
+    ) initializer public {
+        __ERC721_init(name_, symbol_);
+        __baseURI_init(baseURI_);
+        __Ownable_init();
+    }
+
+    function __baseURI_init(string memory baseURI_) internal onlyInitializing {
         baseURI = baseURI_;
     }
 
