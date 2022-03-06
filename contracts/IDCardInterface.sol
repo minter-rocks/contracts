eebff510d6bdadcddf3bfb383553574251c2ca2e
r.nouripoor <r.nouripoor@gmail.com>
2022-03-06 16:55:26 +0330

    IDCard deleted

--- contracts/IDCardInterface.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
-
-interface IDCardInterface {
-    function ownerOf(uint256 tokenId) external view returns (address owner);
-    function username(uint256 _userId) external view returns (string memory _username);
-}
