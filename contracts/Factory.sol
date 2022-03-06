837d7a2976571f7dce99e1e2f93a5a1481c51645
r.nouripoor <r.nouripoor@gmail.com>
2022-03-06 17:03:53 +0330

    Factory: Clones library imported

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -2,7 +2,8 @@
 pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/proxy/Clones.sol";
 
 contract Factory is Ownable {
-
+    using Clones for address;
 }
