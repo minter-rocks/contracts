729e9df101050a6c72ad527f54b468feb83762a0
r.nouripoor <r.nouripoor@gmail.com>
2022-03-07 09:54:04 +0330

    version() function added

--- contracts/NFTCollection.sol
+++ contracts/NFTCollection.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
 
-contract NFT is Initializable, ERC721Upgradeable, ERC721URIStorageUpgradeable, OwnableUpgradeable {
+contract NFTCollection is Initializable, ERC721Upgradeable, ERC721URIStorageUpgradeable, OwnableUpgradeable {
     using CountersUpgradeable for CountersUpgradeable.Counter;
 
     CountersUpgradeable.Counter private _tokenIdCounter;
--- contracts/NFTCollectionFactory.sol
+++ contracts/NFTCollectionFactory.sol
@@ -5,6 +5,11 @@ import "@openzeppelin/contracts/proxy/Clones.sol";
 import "./NFTCollection.sol";
 
 contract NFTFactory {
+
+    function version() public pure returns(string memory V_) {
+        V_ = "0.1";
+    }
+
     using Clones for address;
 
     NFTCollection public NFTLib = new NFTCollection();
