48e9bcc33f4ab75df50379c754cae4b537219e30
r.nouripoor <r.nouripoor@gmail.com>
2022-03-07 09:43:09 +0330

    NFTFactory: Ownable deleted

--- contracts/NFTFactory.sol
+++ contracts/NFTFactory.sol
@@ -1,11 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/proxy/Clones.sol";
 import "./NFTContract.sol";
 
-contract NFTFactory is Ownable {
+contract NFTFactory {
     using Clones for address;
 
     NFTContract public NFTLib = new NFTContract();
