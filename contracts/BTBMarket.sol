59160921478d4b993f72007cc3b1ca8dc233dc65
r.nouripoor <r.nouripoor@gmail.com>
2022-04-06 17:55:03 +0430

    events added

--- contracts/BTBMarket.sol
+++ contracts/BTBMarket.sol
@@ -5,4 +5,7 @@ import "./BanTheBanNFT.sol";
 
 contract BTBMarket is BanTheBanNFT {
 
+    event ForSale(uint256 tokenId, uint256 minValue);
+    event noLongerForSale(uint256 tokenId);
+    event bought(uint256 tokenId, uint256 value, address from, address to);
 }
