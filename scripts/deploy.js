6a34b15cae33904fb8318911f58a14958c4b792d
r.nouripoor <r.nouripoor@gmail.com>
2022-04-08 11:23:48 +0430

    deploy.js set

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,10 +2,10 @@ const { ethers, upgrades } = require("hardhat");
 
   async function main() {
     // simple deploy
-    const NFTFactory = await ethers.getContractFactory("NFTFactory");
-    const Factory = await NFTFactory.deploy();
-    await Factory.deployed();
-    console.log("NFTFactory Contract Address:", Factory.address); 
+    const BTBMarket = await ethers.getContractFactory("BTBMarket");
+    const BTBM = await BTBMarket.deploy();
+    await BTBM.deployed();
+    console.log("BTBMarket Contract Address:", BTBM.address); 
   }
     
   main();
