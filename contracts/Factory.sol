8f5221e168cd8c8fc490d0b951e7651cae19d685
Renope <r.nouripoor@gmail.com>
2022-05-05 02:13:07 +0430

    Factory

---
+++ contracts/Factory.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/proxy/Clones.sol";
+import "./Gallery.sol";
+
+contract NFTFactory is Ownable {
+    using Clones for address;
+
+    Gallery NFT = new Gallery();
+
+    function newNFTContract(
+        string memory name,
+        string memory symbol,
+        string memory baseURI
+    ) public {
+
+    }
+}
