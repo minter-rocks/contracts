13f3d980a46e84ef656f2d843a0a0415f169fea7
Renope <r.nouripoor@gmail.com>
2022-05-05 02:53:18 +0430

    Factory: not ownable

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -1,11 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/proxy/Clones.sol";
 import "./Gallery.sol";
 
-contract NFTFactory is Ownable {
+contract NFTFactory {
     using Clones for address;
 
     Gallery NFT = new Gallery();
