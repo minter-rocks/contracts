661d264c44837e60b80a0feb0e5dd679f5f6485f
Renope <r.nouripoor@gmail.com>
2022-06-19 13:03:21 +0430

    register() completed

--- contracts/3_ChatRoom/ChatRoom.sol
+++ contracts/3_ChatRoom/ChatRoom.sol
@@ -7,7 +7,16 @@ import "./ChatRoomInternal.sol";
 contract ChatRoom is ChatRoomInternal{
     
     
-    function register(string memory _username) public {
-        _register(msg.sender, _username);
+    function register(string memory username_) public {
+        _register(msg.sender, username_);
+    }    
+    
+    function unRegister() public {
+        _unRegister(msg.sender);
+    }
+
+    function username(address userAddr) public view returns(string memory usename_) {
+        usename_ = _username(userAddr);
+        require(bytes(usename_).length > 0, "ChatRoom: query for not registered user");
     }
 }
--- contracts/3_ChatRoom/ChatRoomInternal.sol
+++ contracts/3_ChatRoom/ChatRoomInternal.sol
@@ -12,25 +12,43 @@ contract ChatRoomInternal {
     using LiteralRegex for string;
 
     event Register(address userAddr, string username);
+    event UnRegister(address _userAddr);
 
-    function _register(address userAddr, string memory _username) public {
+    function _register(address userAddr, string memory username_) public {
         ChatRoomStorage.Layout storage l = ChatRoomStorage.layout();
 
         require(
-            _username.isLiteral(),
+            username_.isLiteral(),
             "you can just use numbers(0-9) letters(a-zA-Z) and signs(-._)"
         );
 
-        string memory lower = _username.lower();
+        string memory lower = username_.lower();
 
         require(
             l.registered[lower] == address(0), 
             "ChatRoomInternal: the username has already been registered"
         );
 
-        l.users[userAddr] = _username;
+        l.users[userAddr] = username_;
         l.registered[lower] = userAddr;
 
-        emit Register(userAddr, _username);
+        emit Register(userAddr, username_);
+    }
+
+    /**
+     * @notice delete your username.
+     */
+    function _unRegister(address userAddr) public {
+        ChatRoomStorage.Layout storage l = ChatRoomStorage.layout();
+        delete l.registered[l.users[userAddr].lower()];
+        delete l.users[userAddr];
+        emit UnRegister(userAddr);
+    }
+
+    /**
+     * @notice returns the username of the specified address.
+     */
+    function _username(address userAddr) public view returns(string memory) {
+        return ChatRoomStorage.layout().users[userAddr];
     }
 }
