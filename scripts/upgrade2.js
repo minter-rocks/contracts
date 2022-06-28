f17bca03e73dbc7db144f74fe057073d47ae6261
Renope <r.nouripoor@gmail.com>
2022-06-28 12:12:51 +0430

    Donation Deployed

---
+++ scripts/upgrade2.js
@@ -0,0 +1,54 @@
+/* global describe it before ethers */
+
+const {
+  getSelectors,
+  FacetCutAction
+} = require('./libraries/diamond.js')
+    
+const { assert } = require('chai')
+
+async function deployDonation () { 
+
+  const diamondAddress = "0xB42cE907d99b74578b9913A560B9c5C1A237356b" //diamond on polygon
+  let  diamondCut = await ethers.getContractAt('DiamondCutFacet', diamondAddress)
+
+  let tx
+
+
+  // deploy facet
+  console.log('')
+  console.log('Deploying Donation')
+  const FacetName = "Donation"
+  const cut = []
+  const Facet = await ethers.getContractFactory(FacetName)
+  const facet = await Facet.deploy()
+  await facet.deployed()
+  console.log(`${FacetName} deployed: ${facet.address}`)
+  cut.push({
+    facetAddress: facet.address,
+    action: FacetCutAction.Add,
+    functionSelectors: getSelectors(facet).remove(['init()'])
+  })
+
+  // upgrade diamond with facet
+  console.log('')
+  console.log('Diamond Cut:', cut)
+  let functionCall = facet.interface.encodeFunctionData('init')
+  tx = await diamondCut.diamondCut(cut, facet.address, functionCall)
+  console.log('Diamond cut tx: ', tx.hash)
+  console.log('Completed diamond cut')
+  return facet.address
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+if (require.main === module) {
+  deployDonation()
+    .then(() => process.exit(0))
+    .catch(error => {
+      console.error(error)
+      process.exit(1)
+    })
+}
+
+exports.deployDonation = deployDonation
